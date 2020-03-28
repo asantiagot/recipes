6202fbb565af26b30593a58896d20c5ffa4ff2f3
Jose Santiago <josantorres93@gmail.com>
2020-03-27 23:42:38 -0600

    Added Recipe Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import Recipe from './components/Recipe';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  // IMPORTANT - WARNING
+  // PLACE THESE CONSTANTS IN ENVIRONMENT VARIABLES
+  const APP_ID = 'd87fa8bd';
+  const APP_KEY = '490e64ea83d78ca2982193d85eb48a44';
+  const req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  // IMPORTANT - WARNING
+  
+
+  useEffect(() => {
+    console.log('Effect applied');
+    getRecipes();
+  });
+
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = async () => {
+    const response = await fetch(req);
+    const data = await response.json();
+    console.log(data);
+    setRecipes(data.hits)
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+          <button
+            className="search=button" type="submit"
+            onClick={(event) => {
+              event.preventDefault();
+            }}
+          >
+            Search
+          </button>
+      </form>
+      {recipes.map(index => {
+        return (
+          <Recipe/>
+        );
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
